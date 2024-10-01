a3cc79fe32d413e58e5630b57ca34f3d6c1e7813
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-10-01 00:58:03 -0300

    [feat] Testando mapa ej2
    Signed-off-by: LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>

---
+++ src/assets/data/Mapa/especies-data.js
@@ -0,0 +1,12 @@
+export let especies = [
+    { name: "Afghanistan", value: bubblesize(32358260), color: "#7F38A0 " },
+    { name: "Albania", value: bubblesize(32159880), color: "#2E769F" },
+    { name: "Algeria", value: bubblesize(35980193), color: "#816F28" },
+    { name: "Angola", value: bubblesize(19618432), color: "#816F28" },
+    { name: "Argentina", value: bubblesize(40764561), color: "#364A98" },
+    { name: "Armenia", value: bubblesize(3100236), color: "#2E769F" },
+    { name: "Australia", value: bubblesize(22605732), color: "  #257E7B " },
+    { name: "Austria", value: bubblesize(8413429), color: "#2E769F" },
+    { name: "Azerbaijan", value: bubblesize(9306023), color: "#2E769F" },
+    { name: "Bangladesh", value: bubblesize(150493658), color: "#7F38A0 " },
+];
--- src/pages/Mapa/Mapa.jsx
+++ src/pages/Mapa/Mapa.jsx
@@ -1,9 +1,11 @@
+import { BubbleMap } from "../../components/Mapa/BubbleMap";
 import "../../styles/Mapa/Mapa.css";
 
 function Mapa() {
     return (
         <>
             <span>Mapa vem aqui hihihi</span>
+            <BubbleMap />
         </>
     );
 }
